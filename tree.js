bf3c796a69a76ea2df9a283e474038a859003712
Jonathan Giraldo Mosquera <jonathangiraldomosquera@Jonathans-Air.lan>
2024-12-13 22:14:54 +0000

    changes

--- tree.js
+++ tree.js
@@ -8,37 +8,47 @@ class Node {
 
 class BinarySearchTree {
     constructor(){
-        this.root = null; 
-
+        this.root = null;
     }
-    inser(value){
-        let newNode = new Node(value)
-        if(this.root = newNode){
-            this.root === null;
+    insert(value){
+        const newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
             return this;
-        }else {
-            let current = this.root;
-            while(true){
-                if(value < current.value){
-                
-                    if(current.left === null){
-                        current.left === newNode;
-                        return this;
-                    } 
+        }
+        let current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
                 }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
             }
         }
     }
 }
 
-const tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15)
-tree.root.left = new Node(7);
-tree.root.right.left = new Node(9);
 
-//    10 
-// 5 13 
-// 2 7
+//      10
+//   5     13
+// 2  7  11  16
+
+const tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
 
 console.log(tree)
