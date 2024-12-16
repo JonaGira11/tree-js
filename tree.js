cc93d0d0fc9e29bc120d8abfe66f395f560b2048
Jonathan Giraldo Mosquera <jonathangiraldomosquera@Jonathans-Air.lan>
2024-12-16 23:02:02 +0000

    dfs

--- tree.js
+++ tree.js
@@ -62,6 +62,17 @@ class BinarySearchTree {
         }
         return data;
     }
+    DFSPreOrder (){
+        const data = [];
+        let current = this.root;
+        function traverse(node){
+            data.push(node.value)
+            if(node.left)traverse(node.left);
+            if(node.left)traverse(node.right);
+        }
+        traverse(current)
+return data;
+    }
 }
 
 
@@ -78,4 +89,6 @@ tree.insert(8)
 tree.insert(20)
 
 
-console.log(tree.BFS())
+// console.log(tree.BFS())
+
+console.log()
